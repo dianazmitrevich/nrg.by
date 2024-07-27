975d154e6ce2815c4795afb24829de87ae51e2ec
d <d@MacBook-Pro-d.local>
2024-07-27 16:02:00 +0300

    add tabs block

--- layout/src/js/main.js
+++ layout/src/js/main.js
@@ -22,3 +22,27 @@ document.addEventListener("click", (event) => {
         body.classList.remove("stop-scroll");
     }
 });
+
+let d_tabs = document.querySelectorAll(".d_tabs");
+
+d_tabs.forEach((d_tab) => {
+    let tabs = d_tab.querySelectorAll(".tab");
+
+    tabs.forEach((tab) => {
+        tab.addEventListener("click", () => {
+            let num = parseInt(tab.getAttribute("data-tab"));
+
+            d_tab.querySelectorAll(".tab").forEach((element) => {
+                element.classList.remove("selected");
+            });
+
+            d_tab.parentNode.querySelectorAll(".card").forEach((element) => {
+                element.classList.remove("selected");
+            });
+
+            tab.classList.add("selected");
+            let selectedCard = d_tab.parentNode.querySelector(`.card[data-tab='${num}']`);
+            selectedCard.classList.add("selected");
+        });
+    });
+});
