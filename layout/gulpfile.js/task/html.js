27c63e1e60a82ddc097b5e3fbb17a69e0fd4ea5d
d <d@MacBook-Pro-d.local>
2024-07-27 12:26:54 +0300

    first commit

---
+++ layout/gulpfile.js/task/html.js
@@ -0,0 +1,20 @@
+// обработка html
+const html = () => {
+    return $.gulp
+        .src($.path.html.src) // создает файловый поток чтения, который мы можем потом передать дальше
+        .pipe(
+            $.gp.plumber({
+                errorHandler: $.gp.notify.onError((error) => ({
+                    title: "HTML",
+                    message: error.message,
+                })),
+            })
+        )
+        .pipe($.gp.fileInclude())
+        .pipe($.gp.size({ title: "До сжатия" }))
+        .pipe($.gp.htmlmin($.app.htmlmin))
+        .pipe($.gp.size({ title: "После сжатия" }))
+        .pipe($.gulp.dest($.path.html.dest));
+};
+
+module.exports = html;
---
+++ layout/gulpfile.js/task/pug.js
@@ -0,0 +1,17 @@
+// обработка pug
+const pug = () => {
+    return $.gulp
+        .src($.path.pug.src) // создает файловый поток чтения, который мы можем потом передать дальше
+        .pipe(
+            $.gp.plumber({
+                errorHandler: $.gp.notify.onError((error) => ({
+                    title: "Pug",
+                    message: error.message,
+                })),
+            })
+        )
+        .pipe($.gp.pug($.app.pug))
+        .pipe($.gulp.dest($.path.pug.dest));
+};
+
+module.exports = pug;
